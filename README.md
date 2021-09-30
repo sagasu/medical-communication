15fe4769c25b1f45c9b95b796ca6e1cf22d2224a
Mateusz Kopij <sagasu@gmail.com>
2021-09-30 17:34:42 +0100

    Adding stream dependency and introducing first components.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # medical-communication
-App to help medical organizations to communicate with patients..
+App to help medical organizations to communicate with patients.
+
+For communication I am using [stream](https://getstream.io/) and ...
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {StreamChat} from 'stream-chat'
+import {Chat} from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+import {ChannelListContainer, ChannelContainer} from './components'
+
+const apiKey = '';
+
+const client = StreamChat.getInstance(apiKey);
+
 
 const App = () => {
     return (
-        <div>
-            Med app
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer/>
+                <ChannelContainer/>
+            </Chat>
         </div>
     )
 }
