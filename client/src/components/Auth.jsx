f308576691a94a841ea23d8a0c4d0724b0f4b16c
Mateusz Kopij <sagasu@gmail.com>
2021-10-07 18:47:22 +0100

    adding form to auth screen.

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -5,14 +5,94 @@ import axios from 'axios';
 import signinImage from '../assets/signup.jpg';
 
 const Auth = () => {
-    const [isSignup, setIsSignup] = useState(false);
-
+    const [isSignup, setIsSignup] = useState(true);
+    const handleChange = () => {}
+    const switchMode = () => {
+        setIsSignup((prev) => !prev);
+    }
     return (
         <div className="auth__form-container">
             <div className="auth__form-container_fields">
                 <div className="auth__form-container_fields-content">
                     <p>{isSignup ? 'Sign up': 'Sign in'}</p>
+                    <form onSubmit={() => {}}>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="fullName">Full Name</label>
+                                <input name="fullName" 
+                                    type="text"
+                                    placeholder="Full Name"
+                                    onChange={handleChange}
+                                    required
+                                    />
+                            </div>
+                        )}
+                        <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="username">Username</label>
+                                <input name="username" 
+                                    type="text"
+                                    placeholder="Username"
+                                    onChange={handleChange}
+                                    required
+                                    />
+                        </div>
+
+                        {isSignup && (
+                            <>
+                                <div className="auth__form-container_fields-content_input">
+                                    <label htmlFor="phoneNumber">Phone Number</label>
+                                    <input name="phoneNumber" 
+                                        type="text"
+                                        placeholder="Phone Number"
+                                        onChange={handleChange}
+                                        required
+                                        />
+                                </div>
+
+                                <div className="auth__form-container_fields-content_input">
+                                    <label htmlFor="avatarURL">Avatar URL</label>
+                                    <input name="avatarURL" 
+                                        type="text"
+                                        placeholder="Avatar URL"
+                                        onChange={handleChange}
+                                        required
+                                        />
+                                </div>
+                            </>
+                        )}
+                        <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="password">Password</label>
+                                <input name="password" 
+                                    type="password"
+                                    placeholder="Password"
+                                    onChange={handleChange}
+                                    required
+                                    />
+                        </div>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="confirmPassword">Confirm Password</label>
+                                <input name="confirmPassword" 
+                                    type="password"
+                                    placeholder="Confirm Password"
+                                    onChange={handleChange}
+                                    required
+                                    />
+                            </div>
+                        )}
+                    </form>
+                    <div className="aut__form-container_fields-account">
+                            <p>
+                                {isSignup ? "Already have an account?" : "Don't have an account?"}
+                                <span onClick={switchMode}>
+                                    {isSignup ? 'SignIn' : 'SignUp'}
+                                </span>
+                            </p>
+                    </div>
                 </div>
+            </div>
+            <div className="auth__form-container_image">
+                <img src={signinImage} alt="sign in" />
             </div>
         </div>
     )
